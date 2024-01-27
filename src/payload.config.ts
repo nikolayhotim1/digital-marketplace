1f922f99726968ce2d446b4d223eb288bf2ea0fe
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-01-27 13:24:07 +0300

    feature: modelling our data & enabling uploads

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,12 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import path from 'path'
 import { buildConfig } from 'payload/config'
-import { Users } from './collections/Users'
+import { Users } from './collections/users'
 import dotenv from 'dotenv'
+import { Products } from './collections/products/products'
+import { Media } from './collections/media'
+import { ProductFiles } from './collections/product-files'
+import { Orders } from './collections/orders'
 
 dotenv.config({
 	path: path.resolve(__dirname, '../.env')
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
 	serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-	collections: [Users],
+	collections: [Users, Products, Media, ProductFiles, Orders],
 	routes: {
 		admin: '/sell'
 	},
